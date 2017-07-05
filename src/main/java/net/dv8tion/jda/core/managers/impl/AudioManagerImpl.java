0eb965dd6cbb9b787331ee5f91032fe6592782dc
Florian Spieß <business@minnced.club>
2017-07-05 13:40:19 +0200

    Improved disposing of users and audio managers

--- src/main/java/net/dv8tion/jda/core/managers/impl/AudioManagerImpl.java
+++ src/main/java/net/dv8tion/jda/core/managers/impl/AudioManagerImpl.java
@@ -24,6 +24,7 @@ import net.dv8tion.jda.core.audio.AudioSendHandler;
 import net.dv8tion.jda.core.audio.hooks.ConnectionListener;
 import net.dv8tion.jda.core.audio.hooks.ConnectionStatus;
 import net.dv8tion.jda.core.audio.hooks.ListenerProxy;
+import net.dv8tion.jda.core.entities.Disposable;
 import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.entities.VoiceChannel;
@@ -39,7 +40,7 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 
-public class AudioManagerImpl implements AudioManager
+public class AudioManagerImpl implements AudioManager, Disposable
 {
     //These values are set at the bottom of this file.
     public static boolean AUDIO_SUPPORTED;
@@ -59,6 +60,7 @@ public class AudioManagerImpl implements AudioManager
     protected ListenerProxy connectionListener = new ListenerProxy();
     protected long queueTimeout = 100;
     protected boolean shouldReconnect = true;
+    protected boolean disposed = false;
 
     protected boolean selfMuted = false;
     protected boolean selfDeafened = false;
@@ -347,6 +349,23 @@ public class AudioManagerImpl implements AudioManager
                     );
             api.getClient().send(voiceStateChange.toString());
         }
+    }
+
+    @Override
+    public boolean dispose()
+    {
+        synchronized (CONNECTION_LOCK)
+        {
+            guild = null;
+            closeAudioConnection(ConnectionStatus.DISCONNECTED_REMOVED_FROM_GUILD);
+            return disposed = true;
+        }
+    }
+
+    @Override
+    public boolean isDisposed()
+    {
+        return disposed;
     }
 
     //Load the Opus library.
